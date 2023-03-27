ed130f54614e3e7932b89f7940d95448fcf3612a
josgr <josgrooten@gmail.com>
2023-03-27 11:22:27 +0200

    Added environment.development.ts to changed between dev and prod

--- src/app/dataTransfer/data-transfer.service.ts
+++ src/app/dataTransfer/data-transfer.service.ts
@@ -1,61 +1,60 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Student} from "../model/student.model";
-import {Course} from "../model/course.model";
-import {CourseStatus} from "../model/courseStatus.model";
+import { HttpClient } from "@angular/common/http";
+import { Student } from "../model/student.model";
+import { Course } from "../model/course.model";
+import { CourseStatus } from "../model/courseStatus.model";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTransferService {
 
-  constructor(private http: HttpClient) { }
+  private endpointS = 'student';
+  private endpointC = 'course';
 
-  // Dev:
-//   urlStudent: string = 'http://localhost:8080/student';
-//
-//   urlCourse: string = 'http://localhost:8080/course';
+  private domain: string | undefined;
 
-  // Prod:
-  urlStudent: string = 'https://sap-production.up.railway.app/student';
 
-  urlCourse: string = 'https://sap-production.up.railway.app/course';
+  constructor(private http: HttpClient) {
+    this.domain = environment.domain;
+  }
 
 
   public getStudents() {
-    return this.http.get<Student[]>(`${this.urlStudent}/all`);
+    return this.http.get<Student[]>(`${this.domain}${this.endpointS}/all`);
   }
 
   public addStudent(student: Student) {
-    return this.http.post<Student>(`${this.urlStudent}/add`, student);
+    return this.http.post<Student>(`${this.domain}${this.endpointS}/add`, student);
   }
 
   public editStudent(student: Student) {
-    return this.http.put<Student>(`${this.urlStudent}/update`, student);
+    return this.http.put<Student>(`${this.domain}${this.endpointS}/update`, student);
   }
 
   public deleteStudent(id?: number) {
-    return this.http.delete<void>(`${this.urlStudent}/delete/${id}`);
+    return this.http.delete<void>(`${this.domain}${this.endpointS}/delete/${id}`);
   }
 
   public addCourseStatus(data: any) {
-    return this.http.post<void>(`${this.urlStudent}/setCourseStatus`, data);
+    return this.http.post<void>(`${this.domain}${this.endpointS}/setCourseStatus`, data);
   }
 
   public getCourses() {
-    return this.http.get<Course[]>(`${this.urlCourse}/all`);
+    return this.http.get<Course[]>(`${this.domain}${this.endpointC}/all`);
   }
 
   public addCourse(course: Course) {
-    return this.http.post<Course>(`${this.urlCourse}/add`, course);
+    return this.http.post<Course>(`${this.domain}${this.endpointC}/add`, course);
   }
 
   public deleteCourse(id: number) {
-    return this.http.delete<void>(`${this.urlCourse}/delete/${id}`);
+    return this.http.delete<void>(`${this.domain}${this.endpointC}/delete/${id}`);
   }
 
   public getCourseStatus(id: number) {
-    return this.http.get<CourseStatus[]>(`${this.urlStudent}/findCourseAndStatus/${id}`)
+    return this.http.get<CourseStatus[]>(`${this.domain}${this.endpointS}/findCourseAndStatus/${id}`)
   }
 
 
